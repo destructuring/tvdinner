6678935c4e6d94127e951aa888a3d3716c0e7572
Tom Bombadil <amanibhavam@destructuring.org>
2013-05-31 15:50:14 -0700

    got require tvdinner to work

--- lib/tvdinner.rb
+++ lib/tvdinner.rb
@@ -1,29 +1,31 @@
-class TVDinner
+module TVDinner
 end
 
-class Chef::Config
-  def self.local_config shome
-    sandbox_path platform_specific_path("#{shome}/var/chef/sandboxes")
-    checksum_path platform_specific_path("#{shome}/var/chef/checksums")
-    file_cache_path platform_specific_path("#{shome}/var/chef/cache")
-    file_backup_path platform_specific_path("#{shome}/var/chef/backup")
-    node_path platform_specific_path("#{shome}/var/chef/node")
-    role_path platform_specific_path("#{shome}/var/chef/roles")
-    data_bag_path platform_specific_path("#{shome}/var/chef/data_bags")
-    client_key platform_specific_path("#{shome}/etc/chef/client.pem")
-    validation_key platform_specific_path("#{shome}/etc/chef/validation.pem")
-    signing_ca_cert platform_specific_path("#{shome}/var/chef/ca/cert.pem")
-    signing_ca_key platform_specific_path("#{shome}/var/chef/ca/key.pem")
-    cache_options({ :path => platform_specific_path("#{shome}/var/chef/cache/checksums") })
-  end
+class Chef
+  class Config
+    def self.local_config shome
+      sandbox_path platform_specific_path("#{shome}/var/chef/sandboxes")
+      checksum_path platform_specific_path("#{shome}/var/chef/checksums")
+      file_cache_path platform_specific_path("#{shome}/var/chef/cache")
+      file_backup_path platform_specific_path("#{shome}/var/chef/backup")
+      node_path platform_specific_path("#{shome}/var/chef/node")
+      role_path platform_specific_path("#{shome}/var/chef/roles")
+      data_bag_path platform_specific_path("#{shome}/var/chef/data_bags")
+      client_key platform_specific_path("#{shome}/etc/chef/client.pem")
+      validation_key platform_specific_path("#{shome}/etc/chef/validation.pem")
+      signing_ca_cert platform_specific_path("#{shome}/var/chef/ca/cert.pem")
+      signing_ca_key platform_specific_path("#{shome}/var/chef/ca/key.pem")
+      cache_options({ :path => platform_specific_path("#{shome}/var/chef/cache/checksums") })
+    end
 
-  def self.bundler_config more_paths=[]
-    # look in bundler gems for cookbooks, which are peers to lib/
-    # for every gem/xxx/lib, look for gem/xxx/cookbooks
-    bundler_paths = :.collect do |x|
-      File.join(File.expand_path("..", x), "cookbooks")
-    end.select {|x| File.directory? x } # return existing directories
+    def self.bundler_config more_paths=[]
+      # look in bundler gems for cookbooks, which are peers to lib/
+      # for every gem/xxx/lib, look for gem/xxx/cookbooks
+      bundler_paths = $:.collect do |x|
+        File.join(File.expand_path("..", x), "cookbooks")
+      end.select {|x| File.directory? x } # return existing directories
 
-    cookbook_path = Array(more_paths).concat(bundler_paths)
+      cookbook_path Array(more_paths).concat(bundler_paths)
+    end
   end
 end
