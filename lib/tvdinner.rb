98e0c129315f22c6a866df8212a222ea4dc60e0b
Tom Bombadil <amanibhavam@destructuring.org>
2013-06-01 00:01:44 -0700

    configure and create chef dirs at the same time

--- lib/tvdinner.rb
+++ lib/tvdinner.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 module TVDinner
 end
 
@@ -5,17 +7,37 @@ class Chef
   class Config
     def self.local_config shome
       sandbox_path platform_specific_path("#{shome}/var/chef/sandboxes")
+      ensure_mkdir sandbox_path
+
       checksum_path platform_specific_path("#{shome}/var/chef/checksums")
+      ensure_mkdir checksum_path
+
       file_cache_path platform_specific_path("#{shome}/var/chef/cache")
+      ensure_mkdir file_cache_path
+
       file_backup_path platform_specific_path("#{shome}/var/chef/backup")
+      ensure_mkdir file_backup_path
+
       node_path platform_specific_path("#{shome}/var/chef/node")
+      ensure_mkdir node_path
+
       role_path platform_specific_path("#{shome}/var/chef/roles")
-      data_bag_path platform_specific_path("#{shome}/var/chef/data_bags")
-      client_key platform_specific_path("#{shome}/etc/chef/client.pem")
-      validation_key platform_specific_path("#{shome}/etc/chef/validation.pem")
-      signing_ca_cert platform_specific_path("#{shome}/var/chef/ca/cert.pem")
-      signing_ca_key platform_specific_path("#{shome}/var/chef/ca/key.pem")
+      ensure_mkdir role_path
+
       cache_options({ :path => platform_specific_path("#{shome}/var/chef/cache/checksums") })
+      ensure_mkdir cache_options[:path]
+
+      #data_bag_path platform_specific_path("#{shome}/var/chef/data_bags")
+      #client_key platform_specific_path("#{shome}/etc/chef/client.pem")
+      #validation_key platform_specific_path("#{shome}/etc/chef/validation.pem")
+      #signing_ca_cert platform_specific_path("#{shome}/var/chef/ca/cert.pem")
+      #signing_ca_key platform_specific_path("#{shome}/var/chef/ca/key.pem")
+    end
+
+    def ensure_mkdir d
+      unless File.exists? d
+        FileUtils.mkdir_p d
+      end
     end
 
     def self.bundler_config more_paths=[]
